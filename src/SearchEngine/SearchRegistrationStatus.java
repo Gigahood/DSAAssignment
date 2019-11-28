14ee5bf7b304a085a5e8a4af05a22788aa3aa27b
KUEK YONG BOON <kuekyb@gmail.com>
2019-11-28 23:15:54 +0800

    Finish pagination

--- src/SearchEngine/SearchRegistrationStatus.java
+++ src/SearchEngine/SearchRegistrationStatus.java
@@ -7,6 +7,7 @@ package SearchEngine;
 
 import Constant.ConsoleColors;
 import DataClass.StudentRegistration;
+import DataStructureClass.MyArrayList;
 import DataStructureClass.MyList;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -69,68 +70,108 @@ public class SearchRegistrationStatus {
     }
 
     private void searchByPending() {
+        Main.clearScreen();
+
+        MyList<StudentRegistration> registrationList = Main.db.registerList;
+
+        int length = registrationList.size();
+
+        int length2;
+        int page;
+        int currentPage = 1;
+        int min;
+        int max;
+        int studentIndex;
+
         while (true) {
-            Main.clearScreen();
-            // format for displaying date
-            SimpleDateFormat ft = new SimpleDateFormat("dd.MM.yyyy");
-            String str = "";
-
-            System.out.println("***Searched Result with Pending Status***");
-            System.out.println("");
-            System.out.println("Today Date : " + ft.format(new Date()));
-            System.out.println("");
-            MyList<StudentRegistration> registrationList = Main.db.registerList;
-            int length = registrationList.size();
-            int studentIndex = 1;
-
-            // title of the search result
-            str += String.format("%-10s %-30s %-30s %-20s %-15s\n",
-                    "No.", "Registration ID", "Pending Time", "Register Date", "Status");
+            MyList<StudentRegistration> approvedList = new MyArrayList<>();
 
             for (int i = 0; i < length; i++) {
                 if (registrationList.get(i).getStatus().equals("pending")) {
-                    str += String.format("%-10s %-30s %-30s %-20s %-15s\n",
-                            studentIndex,
-                            registrationList.get(i).getRegistrationID(),
-                            getDayDifference(registrationList.get(i).getRegistrationDate().getTime()) + " days ago",
-                            ft.format(registrationList.get(i).getRegistrationDate()),
-                            ConsoleColors.YELLOW + registrationList.get(i).getStatus() + ConsoleColors.RESET);
-                    studentIndex++;
+                    approvedList.add(registrationList.get(i));
                 }
             }
-            System.out.println(str);
-            System.out.println("");
-            System.out.println("***Total " + (studentIndex - 1) + " Students***");
-            System.out.println("");
-            if (studentIndex == 1) {
-                System.out.println(ConsoleColors.RED_BOLD + "Currently No Pending Student Registration!" + ConsoleColors.RESET);
-                System.out.println("Press Enter To Continue...");
-                Main.scan.nextLine();
+            
+            if (approvedList.isEmpty()) {
+                System.out.println(ConsoleColors.RED_BOLD + "Currently No Pending Student" + ConsoleColors.RESET);
             }
-
+            
+            length2 = approvedList.size();
+            
+            page = getPageSize(length2);
+            min = getMin(currentPage);
+            max = getMax(currentPage, page, length2);
+            studentIndex = getStudentIndex(min);
+            String code;
+            String input;
+            
+            printApprovedList(approvedList, min, max, studentIndex);
             System.out.println("");
-
-            System.out.println("Please enter registration ID to view Student Details");
+            System.out.println("Page : " + currentPage + " / " + page);
+            System.out.println("Total Number of Students : " + length2);
             System.out.println("");
-            System.out.println("*** Enter back or 0 to return *** ");
+            String navigationCode = printNavigation(currentPage, page);
+            
+            //validateNavigationCode(navigationCode);
             System.out.println("");
-            System.out.print("Your selection ---> ");
-            String selection = Main.scan.nextLine();
+            System.out.print("Your Choice --> ");
+            input = Main.scan.nextLine();
+            
+            
+            code = getCode(navigationCode, input);
 
-            if (selection.toUpperCase().equals("BACK") || selection.equals("0")) {
+
+
+            /*
+                1. Next page
+                2. Previous Page
+                3. Approve Student
+                4. Return
+                5. Out of Range
+                6. Empty
+                7. Not number
+             */
+            if (code.equals("4")) {
                 break;
             }
 
-            StudentRegistration student = validateRegistrationID(selection);
+            switch (code) {
+                case "1":
+                    currentPage++;
+                    break;
+                case "2":
+                    currentPage--;
+                    break;
+                case "3":
+                    System.out.println("");
+                    System.out.println("Please enter Registration ID --> ");
+                    String id = Main.scan.nextLine();
 
-            if (student != null) {
-                approveStudent(student);
-            } else {
-                System.out.println(ConsoleColors.RED_BOLD + "No Such Student" + ConsoleColors.RESET);
-                System.out.println("Press enter to continue");
-                Main.scan.nextLine();
+                    StudentRegistration student = validateRegistrationID(id);
+                    if (student != null) {
+                        approveStudent(student);
+                    } else {
+                        System.out.println(ConsoleColors.RED_BOLD + "No Such Student" + ConsoleColors.RESET);
+                        System.out.println("Press enter to continue");
+                        Main.scan.nextLine();
 
+                    }
+                    currentPage = 1;
+                    break;
+                case "5" :
+                    System.out.println(ConsoleColors.BLUE_BOLD + "INVALID INPUT! Please key in number within range!!" + ConsoleColors.RESET);
+                    Main.scan.nextLine();
+                    break;
+                case "6" :
+                    System.out.println(ConsoleColors.RED_BOLD + "Cannot be empty field!" + ConsoleColors.RESET);
+                    Main.scan.nextLine();
+                    break;
+                case "7" :
+                     System.out.println(ConsoleColors.BLUE_BOLD + "INVALID INPUT! Please key in number." + ConsoleColors.RESET);
+                    Main.scan.nextLine();
+                    break;
             }
+            Main.clearScreen();
         }
     }
 
@@ -220,5 +261,173 @@ public class SearchRegistrationStatus {
         days = (todayTime - time) / (1000 * 60 * 60 * 24);
 
         return days;
+    }
+
+    private int getPageSize(int length) {
+        int page = length / 5;
+
+        if (length % 5 != 0) {
+            page++;
+        }
+
+        return page;
+    }
+
+    private void printApprovedList(MyList<StudentRegistration> approvedList, int min, int max, int index) {
+        // title of the search result
+        String str = "";
+        int studentIndex = index;
+        SimpleDateFormat ft = new SimpleDateFormat("dd.MM.yyyy");
+
+        System.out.println("***Searched Result with Pending Status***");
+        System.out.println("");
+        System.out.println("Today Date : " + ft.format(new Date()));
+        System.out.println("");
+
+        str += String.format("%-10s %-30s %-30s %-20s %-15s\n",
+                "No.", "Registration ID", "Pending Time", "Register Date", "Status");
+        for (int i = min; i < max; i++) {
+            str += String.format("%-10s %-30s %-30s %-20s %-15s\n",
+                    studentIndex,
+                    approvedList.get(i).getRegistrationID(),
+                    getDayDifference(approvedList.get(i).getRegistrationDate().getTime()) + " days ago",
+                    ft.format(approvedList.get(i).getRegistrationDate()),
+                    ConsoleColors.YELLOW + approvedList.get(i).getStatus() + ConsoleColors.RESET);
+            studentIndex++;
+        }
+
+        System.out.println(str);
+    }
+
+    private int getMin(int currentPage) {
+        return (currentPage - 1) * 5;
+    }
+
+    private int getMax(int currentPage, int maxPage, int length) {
+        if (currentPage == maxPage) {
+            return length;
+        } else {
+            return currentPage * 5;
+        }
+        //return currentPage * 5;
+    }
+
+    private int getStudentIndex(int min) {
+        return min + 1;
+    }
+
+    private String printNavigation(int currentPage, int maxPage) {
+        if (currentPage == maxPage && currentPage != 1) {
+            System.out.println("1. Previous Page");
+            System.out.println("2. Approve Student");
+            System.out.println("3. Return");
+            return "1";
+        } else if (currentPage == 1 && currentPage != maxPage) {
+            System.out.println("1. Next Page");
+            System.out.println("2. Approve Student");
+            System.out.println("3. Return");
+            return "2";
+        } else if (currentPage == maxPage && currentPage == 1) {
+            System.out.println("1. Approve Student");
+            System.out.println("2. Return");
+            return "3";
+        } else {
+            System.out.println("1. Next Page");
+            System.out.println("2. Previous Page");
+            System.out.println("3. Approve Student");
+            System.out.println("4. Return");
+            return "4";
+        }
+    }
+
+    /*
+    1. Next page
+    2. Previous Page
+    3. Approve Student
+    4. Return
+     */
+    private String getCode(String navigationCode, String input) {
+        String code = "";
+        switch (navigationCode) {
+            case "1":
+                switch (input) {
+                    case "1":
+                        code = "2";
+                        break;
+                    case "2":
+                        code = "3";
+                        break;
+                    case "3":
+                        code = "4";
+                        break;
+                    default:
+                        code = "5";
+                        break;
+                }
+                break;
+            case "2":
+                switch (input) {
+                    case "1":
+                        code = "1";
+                        break;
+                    case "2":
+                        code = "3";
+                        break;
+                    case "3":
+                        code = "4";
+                        break;
+                    default:
+                        code = "5";
+                        break;
+                }
+                break;
+            case "3":
+                switch (input) {
+                    case "1":
+                        code = "3";
+                        break;
+                    case "2":
+                        code = "4";
+                        break;
+                    default:
+                        code = "5";
+                        break;
+                }
+                break;
+            case "4":
+                switch (input) {
+                    case "1":
+                        code = "1";
+                        break;
+                    case "2":
+                        code = "2";
+                        break;
+                    case "3":
+                        code = "3";
+                        break;
+                    case "4":
+                        code = "4";
+                        break;
+                    default:
+                        code = "5";
+                        break;
+                }
+                break;
+//            default:
+//                code = "5";
+//                break;
+        }
+        
+        if (input.isEmpty()) {
+            code = "6";
+        } else {
+            try {
+                int inputInt = Integer.parseInt(input);
+            } catch (Exception e) {
+                code = "7";
+            }
+        }
+        
+        return code;
     }
 }
