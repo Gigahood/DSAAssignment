9b461c6fe5ab06b9b1b2e34252117746bfe04264
E Jet <52629564+seeej-wp17@users.noreply.github.com>
2019-12-12 15:05:06 +0800

    [finishes #169588068] Finish admin-search-edu-background

--- src/SearchEngine/Database.java
+++ src/SearchEngine/Database.java
@@ -41,6 +41,43 @@ public class Database {
         registerList.add(r5);
         studentList.add(s5);
         
+        date.set(2019, 8, 12);
+        Student s6 = new Student("Wong", "Ching Ling", "961226032846", "A-Level", "0124542723", "5, Taman Setali Jaya", "06660", "Bayang", "Johor");
+        StudentRegistration r6 = new StudentRegistration(date.getTime(), "approved", s6);
+        registerList.add(r6);
+        studentList.add(s6);
+        
+        date.set(2019, 8, 20);
+        Student s7 = new Student("Tan", "Zhia Yi", "961224032846", "A-Level", "0124543723", "5, Taman Setali Jaya", "06660", "Bayang", "Johor");
+        StudentRegistration r7 = new StudentRegistration(date.getTime(), "approved", s7);
+        registerList.add(r7);
+        studentList.add(s7);
+        
+        date.set(2019, 8, 12);
+        Student s8 = new Student("Yeoh", "Ming Lin", "961326032846", "Diploma", "0134542723", "5, Taman Setali Jaya", "06660", "Bayang", "Johor");
+        StudentRegistration r8 = new StudentRegistration(date.getTime(), "approved", s8);
+        registerList.add(r8);
+        studentList.add(s8);
+        
+        date.set(2019, 8, 1);
+        Student s9= new Student("Tan", "Zhia Yi", "961224032846", "Diploma", "0114543723", "5, Taman Setali Jaya", "06660", "Bayang", "Johor");
+        StudentRegistration r9= new StudentRegistration(date.getTime(), "approved", s9);
+        registerList.add(r9);
+        studentList.add(s9);
+        
+        date.set(2019, 8, 12);
+        Student s10 = new Student("Tan", "Min Jie", "931326032846", "Form 6", "0164542723", "5, Taman Setali Jaya", "06660", "Bayang", "Johor");
+        StudentRegistration r10 = new StudentRegistration(date.getTime(), "approved", s10);
+        registerList.add(r10);
+        studentList.add(s10);
+        
+        date.set(2019, 8, 1);
+        Student s11= new Student("Wong", "Kar Hor", "921224032846", "Form 6", "0194543723", "5, Taman Setali Jaya", "06660", "Bayang", "Johor");
+        StudentRegistration r11= new StudentRegistration(date.getTime(), "approved", s11);
+        registerList.add(r11);
+        studentList.add(s11);
+        
+        
         for (int i = 6 ; i < 20; i++) {
             date.set(2019, 9, i);
             String firstName = randomFirstName();
