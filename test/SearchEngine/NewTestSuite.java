36586c9029b928e296c8060d803eeaa867d515e5
Kuek Yong Boon <kuekyb@gmail.com>
2019-12-13 11:03:12 +0800

    Unit TEstt Suite

---
+++ test/SearchEngine/NewTestSuite.java
@@ -0,0 +1,39 @@
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package SearchEngine;
+//
+//import org.junit.After;
+//import org.junit.AfterClass;
+//import org.junit.Before;
+//import org.junit.BeforeClass;
+//import org.junit.runner.RunWith;
+//import org.junit.runners.Suite;
+//
+///**
+// *
+// * @author User
+// */
+//@RunWith(Suite.class)
+//@Suite.SuiteClasses({SearchEngine.SearchRegistrationStatusTest.class, SearchEngine.AdminModuleTest.class, SearchEngine.StudentModuleTest.class, SearchEngine.ViewRegistrationDetailTest.class})
+//public class NewTestSuite {
+//
+//    @BeforeClass
+//    public static void setUpClass() throws Exception {
+//    }
+//
+//    @AfterClass
+//    public static void tearDownClass() throws Exception {
+//    }
+//
+//    @Before
+//    public void setUp() throws Exception {
+//    }
+//
+//    @After
+//    public void tearDown() throws Exception {
+//    }
+//    
+//}
